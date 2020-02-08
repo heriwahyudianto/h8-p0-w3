1446a10a30099f23efe2f16032e14219ca3da4d3
heri wahyudianto <heriwahyudianto@gmail.com>
2020-02-08 10:01:27 +0700

    palindrome angka no magic

--- exercise-6.js
+++ exercise-6.js
@@ -1,9 +1,18 @@
 function angkaPalindrome(num) {
   // you can only write your code here!
+  if (num >= 0 && num < 9 ) {return ++num;}
   let palindrome = '';
   while (num.toString() !== palindrome ) {
     num++;
-    palindrome = num.toString().split("").reverse().join("");
+    palindrome = num.toString();
+    let arrPalindrome = [];
+    for (i=0; i < palindrome.length; i++) {
+      arrPalindrome.push(palindrome[i]);
+    }
+    palindrome = '';
+    for (j = arrPalindrome.length - 1; j >= 0; j--) {
+      palindrome += arrPalindrome[j];
+    }
   }
   return palindrome;
 }
